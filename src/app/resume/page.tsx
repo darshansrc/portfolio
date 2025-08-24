bdde2ee974fef4bb9a6e4e81653d716c46818a70
Darshan Gowda <96766940+darshansrc@users.noreply.github.com>
2025-08-24 17:43:35 +0530

    fixes

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 import { DATA } from "@/data/resume";
-import { FileIcon } from "lucide-react";
+import { FileIcon, Loader, Loader2 } from "lucide-react";
 import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 
@@ -31,31 +31,16 @@ export const metadata: Metadata = {
 };
 
 export default function ResumePage() {
-  redirect("/resume/download");
-
   return (
-    <div className="flex max-h-screen  items-center justify-center">
-      <div className="text-center my-auto h-full mt-32 md:mt-64 space-y-4">
-        <FileIcon className="size-10 mx-auto w-full" />
-        <h1 className="text-2xl font-bold">{DATA.name} - Resume</h1>
-        <p className="text-muted-foreground max-w-md mx-auto">
-          {DATA.description}
-        </p>
-        <a
-          href="/resume/download"
-          className="inline-block bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition-colors"
-        >
-          View Resume PDF
-        </a>
-      </div>
+    <div className="flex max-h-screen  w-full items-center justify-center">
+      <Loader className="size-6  mt-10 md:mt-20 text-muted-foreground animate-spin" />
       <script
         dangerouslySetInnerHTML={{
           __html: `
-          // Redirect to PDF after a brief moment to allow social media crawlers to parse metadata
-          setTimeout(() => {
-            window.location.href = '/resume/download';
-          }, 2000);
-        `,
+        setTimeout(() => {
+          window.location.href = '/resume.pdf';
+        }, 1000);
+      `,
         }}
       />
     </div>
